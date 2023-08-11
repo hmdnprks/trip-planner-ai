599f916fd79589e2de9dcbec197e2ff31a5ad4b5
hmdnprks <hamdan.prakoso@mail.ugm.ac.id>
2023-08-11 14:50:25 +0700

    add security headers

--- next.config.js
+++ next.config.js
@@ -1,46 +1,46 @@
 /** @type {import('next').NextConfig} */
 
-// const securityHeaders = [
-//   {
-//     key: 'X-Frame-Options',
-//     value: 'SAMEORIGIN',
-//   },
-//   {
-//     key: 'Content-Security-Policy',
-//     value: `frame-ancestors 'none'`,
-//   },
-//   {
-//     key: 'Strict-Transport-Security',
-//     value: 'max-age=63072000; includeSubDomains; preload',
-//   },
-//   {
-//     key: 'X-XSS-Protection',
-//     value: '1; mode=block',
-//   },
-//   {
-//     key: 'X-Content-Type-Options',
-//     value: 'nosniff',
-//   },
-//   {
-//     key: 'Referrer-Policy',
-//     value: 'same-origin',
-//   },
-//   {
-//     key: 'Permissions-Policy',
-//     value: 'camera=(self)',
-//   },
-// ];
+const securityHeaders = [
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: `frame-ancestors 'none'`,
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'same-origin',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(self)',
+  },
+];
 
 const nextConfig = {
   reactStrictMode: true,
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/(.*)',
-  //       headers: securityHeaders,
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
